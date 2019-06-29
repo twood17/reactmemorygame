e89e7d916a50cc11a39a103ff35f5e4212c53564
Ty Wood <twood06@gmail.com>
2019-06-29 10:44:04 -0400

    Updates

---
+++ precache-manifest.2d7fc49c00b0d7f1f517f09e9217d2b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc896072afa3a646a472f9c3d7d54ac2",
+    "url": "/reactmemorygame/index.html"
+  },
+  {
+    "revision": "3c046e93a8c2439c285b",
+    "url": "/reactmemorygame/static/css/main.ae520c67.chunk.css"
+  },
+  {
+    "revision": "00063f64c68494124901",
+    "url": "/reactmemorygame/static/js/2.49afd6f5.chunk.js"
+  },
+  {
+    "revision": "3c046e93a8c2439c285b",
+    "url": "/reactmemorygame/static/js/main.f8530b02.chunk.js"
+  },
+  {
+    "revision": "834b4ac589a3a1aeb945",
+    "url": "/reactmemorygame/static/js/runtime~main.e9c274db.js"
+  }
+]);
